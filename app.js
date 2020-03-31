c087f1490fcdd8b1695a70295857fcf68aec04bb
Chris-Bortel <bortelcc19@gmail.com>
2020-03-31 14:37:41 -0700

    layed out html and started js

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// 5 questions about myself
+// 1. Am I am web developer
+var webDeveloper = confirm('Am I a web developer ');
+console.log("yes: " + webDeveloper);
+// 2. Do I have a dog? ... Named maddie
+var haveDog = 
+// 3. Did I have a prior career in education?
+// 4. Did I break my thumb by tripping on my own foot?
+// 5. Do I love Star Wars?
+
+
+// y starts with

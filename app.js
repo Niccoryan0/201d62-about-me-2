8c138049dc0bedbea19e811097eb413c146caf21
Chris-Bortel <bortelcc19@gmail.com>
2020-04-01 15:48:13 -0700

    finished part 2 is complete

--- app.js
+++ app.js
@@ -1,83 +1,85 @@
 'use strict' // requires you to use correct notation. Syntax checker is less forgiving. cd
 
-// 5 questions about myself
+// // 5 questions about myself
 
-// welcome message
-var welcome = prompt('Welcome, please tell me your name. ');
-// alert('hello ' + welcome);
-console.log('userName ' + welcome);
-// 1. Am I am web developer
-var webDeveloper = prompt('Am I a web developer? ');
-console.log('Am I a web developer: ' + webDeveloper);
-if (webDeveloper === 'yes') {
-  console.log( 'yes I am a web developer');
-  // console.log('I am a web developer');
-} else {
-  // alert('Nope, I am a web developer' );
-  console.log('Nope, I am a web developer' );
-  // console.log('I am a web developer' );
-};
-// 2. Do I have a dog? ... Named maddie
-var haveDog = prompt('Do I have a dog?');
-console.log('haveDog: ' + haveDog);
-if (haveDog === 'yes') {
-  // alert('I have a dog named Maddie');
-  console.log('I have a dog named Maddie');
-} else {
-  // alert('I have a dog named Maddie')
-  console.log('I have a dog named Maddie')
-};
-// 3. Did I have a prior career in education?
-var priorCareer = prompt('Did I have a prior career in education?');
-console.log('I had a career in education: ' + priorCareer);
-if (haveDog === 'yes') {
-  // alert('I did have a career in education');
-  console.log('I did have a career in education');
-} else {
-  // alert('I did have a career in education')
-  console.log('I did have a career in education')
-};
-// 4. Did I break my thumb by tripping on my own foot?
-var brokenThumb = prompt('Did I break my thumb by tripping on my own foot??');
-console.log('I broke my thumb: ' + brokenThumb);
-if (brokenThumb === 'yes') {
-  // alert('I did break my ');
-  console.log('I did break my ');
-} else {
-  // alert('Unfortunately, I did break my thumb.')
-  console.log('Unfortunately, I did break my thumb.')
-};
-// 5. Is my goal to become a front end react developer
-var careerGoal = prompt('Is Is my goal to become a front end react developer');
-console.log('I want to be a frontend react developer');
-if(careerGoal === 'yes') {
-  // alert('I want to be a frontend react developer ');
-  console.log('I want to be a frontend react developer ');
-};
+// // welcome message
+// var welcome = prompt('Welcome, please tell me your name. ');
+// // alert('hello ' + welcome);
+// console.log('userName ' + welcome);
+// // 1. Am I am web developer
+// var webDeveloper = prompt('Am I a web developer? ');
+// console.log('Am I a web developer: ' + webDeveloper);
+// if (webDeveloper === 'yes') {
+//   console.log( 'yes I am a web developer');
+//   // console.log('I am a web developer');
+// } else {
+//   // alert('Nope, I am a web developer' );
+//   console.log('Nope, I am a web developer' );
+//   // console.log('I am a web developer' );
+// };
+// // 2. Do I have a dog? ... Named maddie
+// var haveDog = prompt('Do I have a dog?');
+// console.log('haveDog: ' + haveDog);
+// if (haveDog === 'yes') {
+//   // alert('I have a dog named Maddie');
+//   console.log('I have a dog named Maddie');
+// } else {
+//   // alert('I have a dog named Maddie')
+//   console.log('I have a dog named Maddie')
+// };
+// // 3. Did I have a prior career in education?
+// var priorCareer = prompt('Did I have a prior career in education?');
+// console.log('I had a career in education: ' + priorCareer);
+// if (haveDog === 'yes') {
+//   // alert('I did have a career in education');
+//   console.log('I did have a career in education');
+// } else {
+//   // alert('I did have a career in education')
+//   console.log('I did have a career in education')
+// };
+// // 4. Did I break my thumb by tripping on my own foot?
+// var brokenThumb = prompt('Did I break my thumb by tripping on my own foot??');
+// console.log('I broke my thumb: ' + brokenThumb);
+// if (brokenThumb === 'yes') {
+//   // alert('I did break my ');
+//   console.log('I did break my ');
+// } else {
+//   // alert('Unfortunately, I did break my thumb.')
+//   console.log('Unfortunately, I did break my thumb.')
+// };
+// // 5. Is my goal to become a front end react developer
+// var careerGoal = prompt('Is Is my goal to become a front end react developer');
+// console.log('I want to be a frontend react developer');
+// if(careerGoal === 'yes') {
+//   // alert('I want to be a frontend react developer ');
+//   console.log('I want to be a frontend react developer ');
+// };
 // 6. Guess my favorite number from these four options. 
 //TODO: Should 10 be a string?
 var favoriteNumber = 10;
-var favoriteNumberAttemps = 4;
+var userAttempts = 4;
 var favoriteNumberQuestion = prompt('Guess my favorite number between 1 and 20: you have four tries');
 // I want to give user 4 tries
 // TODO: make for loop
 // TODO: do while loop.
 
-
-// for(var i = 0; i < favoriteNumberAttemps; i++) {
-//   if(favoriteNumberResponse === 10) {
-//     alert('10 is my favorite number!'); //Why is this not working!?
-//     //too low
-//   } else if(favoriteNumberResponse < 10) {
-//     prompt('your guess was too low, please try again');
-//     //too high
-//   } else if(favoriteNumberResponse > 10) {
-//     prompt('your guess was too high, please try again');
-//   } else {
-//     prompt('please choose a number between 1 and 20')
-//   }
-  
-// }
+//To put string into an integer use parse int
+//stop a for loop early
+for(var i = 0; i < userAttempts; i++) {
+  console.log(favoriteNumberQuestion);
+  if(parseInt(favoriteNumberQuestion) === 10) {
+    alert('10 is my favorite number!'); 
+    break//This only works if you guess the number on the first try...
+    //too low
+  } else if(parseInt(favoriteNumberQuestion) < 10) {
+   favoriteNumberQuestion = prompt('your guess was too low, please try again');
+    //too high
+  } else if(parseInt(favoriteNumberQuestion) > 10) {
+    favoriteNumberQuestion = prompt('your guess was too high, please try again');
+  } else {
+    favoriteNumberQuestion = prompt('please choose a number between 1 and 20')
+  }
+}
 // TODO: If they guess 10, I want to tell them that "10 is my favorite number"
 // TODO: If their guess is lower than 10, I will alert them "your guess was too low, please try again"
 // TODO: If their guess is higher than 10, I will alert them "your guess was too high, please try again"

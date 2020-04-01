9a8ee88fc35880317fb2eaff32b998d826d5f171
Chris-Bortel <bortelcc19@gmail.com>
2020-04-01 13:00:01 -0700

    added ordered list of ten

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict' // requires you to use correct notation. Syntax checker is less forgiving. cd
 
 // 5 questions about myself
 
@@ -10,10 +10,11 @@ console.log('userName ' + welcome);
 var webDeveloper = prompt('Am I a web developer? ');
 console.log('Am I a web developer: ' + webDeveloper);
 if (webDeveloper === 'yes') {
-  alert( 'yes I am a web developer');
+  console.log( 'yes I am a web developer');
   // console.log('I am a web developer');
 } else {
   alert('Nope, I am a web developer' );
+  console.log('Nope, I am a web developer' );
   // console.log('I am a web developer' );
 };
 // 2. Do I have a dog? ... Named maddie
@@ -21,30 +22,37 @@ var haveDog = prompt('Do I have a dog?');
 console.log('haveDog: ' + haveDog);
 if (haveDog === 'yes') {
   alert('I have a dog named Maddie');
+  console.log('I have a dog named Maddie');
 } else {
   alert('I have a dog named Maddie')
+  console.log('I have a dog named Maddie')
 };
 // 3. Did I have a prior career in education?
 var priorCareer = prompt('Did I have a prior career in education?');
 console.log('I had a career in education: ' + priorCareer);
 if (haveDog === 'yes') {
   alert('I did have a career in education');
+  console.log('I did have a career in education');
 } else {
   alert('I did have a career in education')
+  console.log('I did have a career in education')
 };
 // 4. Did I break my thumb by tripping on my own foot?
 var brokenThumb = prompt('Did I break my thumb by tripping on my own foot??');
 console.log('I broke my thumb: ' + brokenThumb);
 if (brokenThumb === 'yes') {
   alert('I did break my ');
+  console.log('I did break my ');
 } else {
   alert('Unfortunately, I did break my thumb.')
+  console.log('Unfortunately, I did break my thumb.')
 };
 // 5. Is my goal to become a front end react developer
 var careerGoal = prompt('Is Is my goal to become a front end react developer');
 console.log('I want to be a frontend react developer');
 if(careerGoal === 'yes') {
   alert('I want to be a frontend react developer ');
+  console.log('I want to be a frontend react developer ');
 };
 
 

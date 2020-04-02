24d0212f2f42730716d4ab7b3f8e70c61f03716d
Chris-Bortel <bortelcc19@gmail.com>
2020-04-01 18:43:37 -0700

    all of the js is working! It could use some styling

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (webDeveloper === 'yes') {
   scoreCounter++;
   // console.log('I am a web developer');
 } else {
-  // alert('Nope, I am a web developer' );
+  alert('Nope, I am a web developer' );
   console.log('Nope, I am a web developer' );
   // console.log('I am a web developer' );
 };
@@ -23,35 +23,39 @@ if (webDeveloper === 'yes') {
 var haveDog = prompt('Do I have a dog?');
 console.log('haveDog: ' + haveDog);
 if (haveDog === 'yes') {
-  // alert('I have a dog named Maddie');
+  alert('I have a dog named Maddie');
   console.log('I have a dog named Maddie');
+  scoreCounter++;
 } else {
-  // alert('I have a dog named Maddie')
+  alert('I have a dog named Maddie')
   console.log('I have a dog named Maddie')
 };
 // 3. Did I have a prior career in education?
 var priorCareer = prompt('Did I have a prior career in education?');
 console.log('I had a career in education: ' + priorCareer);
 if (haveDog === 'yes') {
-  // alert('I did have a career in education');
+  alert('I did have a career in education');
   console.log('I did have a career in education');
+  scoreCounter++;
 } else {
-  // alert('I did have a career in education')
+  alert('I did have a career in education')
   console.log('I did have a career in education')
 };
 // 4. Did I break my thumb by tripping on my own foot?
 var brokenThumb = prompt('Did I break my thumb by tripping on my own foot??');
 console.log('I broke my thumb: ' + brokenThumb);
 if (brokenThumb === 'yes') {
-  // alert('I did break my ');
+  alert('I did break my ');
   console.log('I did break my ');
+  scoreCounter++;
 } else {
-  // alert('Unfortunately, I did break my thumb.')
+  alert('Unfortunately, I did break my thumb.')
   console.log('Unfortunately, I did break my thumb.')
 };
 // 5. Is my goal to become a front end react developer
 var careerGoal = prompt('Is Is my goal to become a front end react developer');
 console.log('I want to be a frontend react developer');
+scoreCounter++;
 if(careerGoal === 'yes') {
   alert('I want to be a frontend react developer ');
   console.log('I want to be a frontend react developer ');
@@ -63,21 +67,22 @@ var favoriteNumber = 10;
 var userAttempts = 4;
 var favoriteNumberQuestion = prompt('Guess my favorite number between 1 and 20: you have four tries');
 
-for(var i = 0; i < userAttempts; i++) {
+for(var i = 1; i < userAttempts; i++) {
   console.log(favoriteNumberQuestion);
   if(parseInt(favoriteNumberQuestion) === 10) {
-    alert('10 is my favorite number!'); 
+    alert('Congratulations!'); 
+    scoreCounter++;
     break;//Stops the loop
   } else if(parseInt(favoriteNumberQuestion) < 10) {
    favoriteNumberQuestion = prompt('your guess was too low, please try again');
     //too high
   } else if(parseInt(favoriteNumberQuestion) > 10) {
     favoriteNumberQuestion = prompt('your guess was too high, please try again');
-  } else {
-    favoriteNumberQuestion = prompt('please choose a number between 1 and 20')
+  } 
   //TODO: need to tell them that they did not guess correct and tell them the answer
-  }
 }
+alert('My favorite number is actually 10')
+
 
 // 7. What is my favorite harry potter book?
 var favBook = ' Sorcerer Stone '
@@ -88,12 +93,13 @@ var favBookQuestion = prompt('What is my favorite Harry Potter Book? from these 
 for(var i = 0; i < bookGuesses; i++) {
 if(favBookQuestion === harryPotterBooks[0].toLowerCase()) {
   alert('Harry Potter and the Sorcerers Stone is my favorite book');
+  scoreCounter++;
   break;
 } else if (i < bookGuesses - 1){ //This takes the amount of guesses index and make it i of 5
   favBookQuestion = prompt('That is not the one, try again!');
 } else { 
-  alert('Whoops, my favorite Harry Potter book is actually Harry Potter and the Sorcerers Stone')
+  alert('Whoops, my favorite Harry Potter book is actually Harry Potter and the Sorcerers')
 }
 }
 
-
+alert('you got: ' + scoreCounter + ' points out of 7 available points');
